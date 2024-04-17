0db0281d300c1d96d283980f1cf99de82ebc9b99
Bhargava Sai <bhargavasai.rayalla@gmail.com>
2024-04-17 10:36:39 +0530

    Version1

--- frontend/src/apis/axiosConfig.js
+++ frontend/src/apis/axiosConfig.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import {hostname} from '../constant/constant.js'
 export const  api=axios.create({
-    baseURL:`http://${hostname}:5000`,
+    baseURL:`https://e-attendance.onrender.com`,
 })
